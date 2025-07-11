1a07fa1d8b362694d49134dc863872151074a840
andre-stack2486 <povoaandre35@gmail.com>
2025-07-11 17:23:37 -0300

    Versão-2
    +Models
    +Banco de Dados
    +Crispy Forms
    +Função Visualizar
    +Função Editar
    +Função Excluir
    +Função Adicionar

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Tarefas
+from .forms import TarefaForm
 
 # Create your views here.
 def listaTarefa(request):
-    return render(request, 'tarefas/list.html')
+    tarefas_list = Tarefas.objects.all().order_by('-created_at')
+    return render(request, 'tarefas/list.html', {'tarefas': tarefas_list})
+
+
+def novaTarefa(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        form.save()
+        return redirect('/')
+    
+    else:
+        form = TarefaForm()
+        return render(request, 'tarefas/addTarefa.html', {'form':form})
+    
+def tarefaView(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    return render (request, 'tarefas/tarefa.html', {'tarefa':tarefa})
+
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefaForm(instance=tarefa)
+
+    if(request.method == 'POST'):
+        form = TarefaForm(request.POST, instance=tarefa)
+
+        if(form.is_valid()):
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+        
+    else:
+        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})   
+def deleteTarefa(request, id):
+     tarefa = get_object_or_404(Tarefas, pk=id)
+     tarefa.delete()
+     return redirect('/')
