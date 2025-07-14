f3ece8771cba54cc8c3e92196598ed2fd050092e
andre-stack2486 <povoaandre35@gmail.com>
2025-07-14 17:21:52 -0300

    Versão-3
    +Login
    +Cadastro
    +Logout

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefas
 from .forms import TarefaForm
+from django.contrib.auth.decorators import  login_required
 
 # Create your views here.
+@login_required
 def listaTarefa(request):
     tarefas_list = Tarefas.objects.all().order_by('-created_at')
     return render(request, 'tarefas/list.html', {'tarefas': tarefas_list})
 
-
+@login_required
 def novaTarefa(request):
     if request.method == 'POST':
         form = TarefaForm(request.POST)
@@ -17,11 +19,14 @@ def novaTarefa(request):
     else:
         form = TarefaForm()
         return render(request, 'tarefas/addTarefa.html', {'form':form})
-    
+
+@login_required   
 def tarefaView(request, id):
     tarefa = get_object_or_404(Tarefas, pk=id)
     return render (request, 'tarefas/tarefa.html', {'tarefa':tarefa})
 
+
+@login_required
 def editTarefa(request, id):
     tarefa = get_object_or_404(Tarefas, pk=id)
     form = TarefaForm(instance=tarefa)
@@ -36,7 +41,9 @@ def editTarefa(request, id):
             return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
         
     else:
-        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})   
+        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+
+@login_required
 def deleteTarefa(request, id):
      tarefa = get_object_or_404(Tarefas, pk=id)
      tarefa.delete()
